3d96114226241eb5fab882e931fdbd1f58ad6ff7
dazraf <farzad.pezeshkpour@gmail.com>
2016-09-05 22:37:25 +0100

    added tuple5 for Futures and Tuple

--- src/main/java/io/dazraf/vertx/futures/Futures.java
+++ src/main/java/io/dazraf/vertx/futures/Futures.java
@@ -5,6 +5,7 @@ import io.dazraf.vertx.tuple.Tuple;
 import io.dazraf.vertx.tuple.Tuple2;
 import io.dazraf.vertx.tuple.Tuple3;
 import io.dazraf.vertx.tuple.Tuple4;
+import io.dazraf.vertx.tuple.Tuple5;
 import io.vertx.core.Future;
 
 /**
@@ -17,15 +18,29 @@ public interface Futures<T> extends Future<T> {
     return FuturesImpl.when(future);
   }
 
-  static <T1, T2> Futures<Tuple2<T1, T2>> when(Future<T1> f1, Future<T2> f2) {
+  static <T1, T2> Futures<Tuple2<T1, T2>> when(Future<T1> f1,
+                                               Future<T2> f2) {
     return FuturesImpl.when(Tuple.tuple(f1, f2));
   }
 
-  static <T1, T2, T3> Futures<Tuple3<T1, T2, T3>> when(Future<T1> f1, Future<T2> f2, Future<T3> f3) {
+  static <T1, T2, T3> Futures<Tuple3<T1, T2, T3>> when(Future<T1> f1,
+                                                       Future<T2> f2,
+                                                       Future<T3> f3) {
     return FuturesImpl.when(Tuple.tuple(f1, f2, f3));
   }
 
-  static <T1, T2, T3, T4> Futures<Tuple4<T1, T2, T3, T4>> when(Future<T1> f1, Future<T2> f2, Future<T3> f3, Future<T4> f4) {
+  static <T1, T2, T3, T4> Futures<Tuple4<T1, T2, T3, T4>> when(Future<T1> f1,
+                                                               Future<T2> f2,
+                                                               Future<T3> f3,
+                                                               Future<T4> f4) {
     return FuturesImpl.when(Tuple.tuple(f1, f2, f3, f4));
+  }
+
+  static <T1, T2, T3, T4, T5> Futures<Tuple5<T1, T2, T3, T4, T5>> when(Future<T1> f1,
+                                                                       Future<T2> f2,
+                                                                       Future<T3> f3,
+                                                                       Future<T4> f4,
+                                                                       Future<T5> f5) {
+    return FuturesImpl.when(Tuple.tuple(f1, f2, f3, f4, f5));
   }
 }
--- src/main/java/io/dazraf/vertx/tuple/Tuple.java
+++ src/main/java/io/dazraf/vertx/tuple/Tuple.java
@@ -28,7 +28,7 @@ public abstract class Tuple<T extends Tuple> {
   }
 
   /**
-   * Create a {@link Tuple3} from two values
+   * Create a {@link Tuple3} from three values
    * @param t1 The first value for the tuple
    * @param t2 The second value for the tuple
    * @param t3 The third value for the tuple
@@ -42,7 +42,7 @@ public abstract class Tuple<T extends Tuple> {
   }
 
   /**
-   * Create a {@link Tuple3} from two values
+   * Create a {@link Tuple4} from four values
    * @param t1 The first value for the tuple
    * @param t2 The second value for the tuple
    * @param t3 The third value for the tuple
@@ -58,8 +58,25 @@ public abstract class Tuple<T extends Tuple> {
   }
 
   /**
-   * Given two futures, of types {@link T1} and {@link T2} respectively, returns a Future of {@link Tuple2} for the
-   * respective types {@link T1} and {@link T2}
+   * Create a {@link Tuple5} from five values
+   * @param t1 The first value for the tuple
+   * @param t2 The second value for the tuple
+   * @param t3 The third value for the tuple
+   * @param t4 The fourth value for the tuple
+   * @param t5 The fifth value for the tuple
+   * @param <T1> The type of the first value
+   * @param <T2> The type of the second value
+   * @param <T3> The type of the third value
+   * @param <T4> The type of the fourth value
+   * @param <T4> The type of the fifth value
+   * @return The constructed {@link Tuple5}
+   */
+  public static <T1, T2, T3, T4, T5> Tuple5<T1, T2, T3, T4, T5> tuple(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5) {
+    return new Tuple5<>(t1, t2, t3, t4, t5);
+  }
+  /**
+   * Given two futures, of types {@link T1} and {@link T2} respectively,
+   * returns a Future of {@link Tuple2} for the respective types {@link T1} and {@link T2}
    *
    * @param f1 The first future of type {@link T1}. The future may be complete or not
    * @param f2 The second future of type {@link T2}. The future may be complete or not
@@ -72,7 +89,8 @@ public abstract class Tuple<T extends Tuple> {
   }
 
   /**
-   * Given three futures, of types {@link T1}, {@link T2} and {@link T3} respectively, returns a Future of {@link Tuple2} for the
+   * Given three futures, of types {@link T1}, {@link T2} and {@link T3} respectively,
+   * returns a Future of {@link Tuple3} for the
    * respective types {@link T1} {@link T2} and {@link T3}
    *
    * @param f1 The first future of type {@link T1}. The future may be complete or not
@@ -81,28 +99,59 @@ public abstract class Tuple<T extends Tuple> {
    * @param <T1> The parameterised type of the {@link Future} {@code f1}
    * @param <T2> The parameterised type of the {@link Future} {@code f2}
    * @param <T3> The parameterised type of the {@link Future} {@code f3}
-   * @return A {@link Future} that will resolve when both {@code f1} {@code f2} {@code f3} have resolved
+   * @return A {@link Future} that will resolve when all
+   * {@code f1} {@code f2} {@code f3} have resolved
    */
   public static <T1, T2, T3> Future<Tuple3<T1, T2, T3>> tuple(Future<T1> f1, Future<T2> f2, Future<T3> f3) {
     return allFutureX(Arrays.asList(f1, f2, f3), Tuple3::new);
   }
 
   /**
-   * Given three futures, of types {@link T1}, {@link T2}, {@link T3} and {@link T4} respectively, returns a Future of {@link Tuple2} for the
-   * respective types {@link T1} {@link T2} and {@link T3}
+   * Given four futures, of types {@link T1}, {@link T2}, {@link T3} and {@link T4} respectively,
+   * returns a Future of {@link Tuple4} for the
+   * respective types {@link T1} {@link T2} {@link T3} {@link T4}
+   *
+   * @param f1 The first future of type {@link T1}. The future may be complete or not
+   * @param f2 The second future of type {@link T2}. The future may be complete or not
+   * @param f3 The third future of type {@link T3}. The future may be complete or not
+   * @param f4 The fourth future of type {@link T4}. The future may be complete or not
+   * @param <T1> The parameterised type of the {@link Future} {@code f1}
+   * @param <T2> The parameterised type of the {@link Future} {@code f2}
+   * @param <T3> The parameterised type of the {@link Future} {@code f3}
+   * @param <T4> The parameterised type of the {@link Future} {@code f4}
+   * @return A {@link Future} that will resolve when all
+   * {@code f1} {@code f2} {@code f3} {@code f4} have resolved
+   */
+  public static <T1, T2, T3, T4> Future<Tuple4<T1, T2, T3, T4>> tuple(Future<T1> f1,
+                                                                      Future<T2> f2,
+                                                                      Future<T3> f3,
+                                                                      Future<T4> f4) {
+    return allFutureX(Arrays.asList(f1, f2, f3, f4), Tuple4::new);
+  }
+
+  /**
+   * Given five futures, of types {@link T1}, {@link T2}, {@link T3}, {@link T4} and {@link T5}
+   * respectively, returns a Future of {@link Tuple5} for the
+   * respective types {@link T1} {@link T2} {@link T3} {@link T4} {@link T5}
    *
    * @param f1 The first future of type {@link T1}. The future may be complete or not
    * @param f2 The second future of type {@link T2}. The future may be complete or not
    * @param f3 The third future of type {@link T3}. The future may be complete or not
    * @param f4 The third future of type {@link T4}. The future may be complete or not
+   * @param f5 The third future of type {@link T5}. The future may be complete or not
    * @param <T1> The parameterised type of the {@link Future} {@code f1}
    * @param <T2> The parameterised type of the {@link Future} {@code f2}
    * @param <T3> The parameterised type of the {@link Future} {@code f3}
    * @param <T4> The parameterised type of the {@link Future} {@code f4}
-   * @return A {@link Future} that will resolve when both {@code f1} {@code f2} {@code f3} {@code f4} have resolved
+   * @param <T5> The parameterised type of the {@link Future} {@code f5}
+   * @return A {@link Future} that will resolve when all {@code f1} {@code f2} {@code f3} {@code f4} {@code f5} have resolved
    */
-  public static <T1, T2, T3, T4> Future<Tuple4<T1, T2, T3, T4>> tuple(Future<T1> f1, Future<T2> f2, Future<T3> f3, Future<T4> f4) {
-    return allFutureX(Arrays.asList(f1, f2, f3, f4), Tuple4::new);
+  public static <T1, T2, T3, T4, T5> Future<Tuple5<T1, T2, T3, T4, T5>> tuple(Future<T1> f1,
+                                                                              Future<T2> f2,
+                                                                              Future<T3> f3,
+                                                                              Future<T4> f4,
+                                                                              Future<T5> f5) {
+    return allFutureX(Arrays.asList(f1, f2, f3, f4, f5), Tuple5::new);
   }
 
   private static <T extends Tuple<T>> Future<T> allFutureX(List<Future> futures, Function<CompositeFuture, T> adapter) {
