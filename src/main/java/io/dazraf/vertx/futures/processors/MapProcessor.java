0e62e0e698aea89e3ccabfb7968d9fd5419b063f
dazraf <farzad.pezeshkpour@gmail.com>
2016-09-05 22:40:12 +0100

    added tuple5 to MapProcessor

--- src/main/java/io/dazraf/vertx/futures/processors/MapProcessor.java
+++ src/main/java/io/dazraf/vertx/futures/processors/MapProcessor.java
@@ -5,9 +5,11 @@ import java.util.function.Function;
 import io.dazraf.vertx.function.Function2;
 import io.dazraf.vertx.function.Function3;
 import io.dazraf.vertx.function.Function4;
+import io.dazraf.vertx.function.Function5;
 import io.dazraf.vertx.tuple.Tuple2;
 import io.dazraf.vertx.tuple.Tuple3;
 import io.dazraf.vertx.tuple.Tuple4;
+import io.dazraf.vertx.tuple.Tuple5;
 import io.vertx.core.AsyncResult;
 
 import static io.vertx.core.Future.failedFuture;
@@ -51,6 +53,10 @@ public interface MapProcessor<T, R> extends FutureProcessor<T, R> {
   }
 
   static <T1, T2, T3, T4, R> MapProcessor<Tuple4<T1, T2, T3, T4>, R> map(Function4<T1, T2, T3, T4, R> function) {
+    return map(tuple -> tuple.apply(function));
+  }
+
+  static <T1, T2, T3, T4, T5, R> MapProcessor<Tuple5<T1, T2, T3, T4, T5>, R> map(Function5<T1, T2, T3, T4, T5, R> function) {
     return map(tuple -> tuple.apply(function));
   }
 }
